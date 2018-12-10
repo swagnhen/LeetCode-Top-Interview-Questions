57cf448f95008c42e8efb7600f1aba74572652a3
swegnhan <1093541368@qq.com>
2018-12-10 10:55:44 +0800

    2018.12.10 commit

--- src/pers/swegnhan/leetcode/medium/math/FractionToRecurringDecimal.java
+++ src/pers/swegnhan/leetcode/medium/math/FractionToRecurringDecimal.java
@@ -4,17 +4,49 @@ import java.util.*;
 
 public class FractionToRecurringDecimal {
 
-    public String fractionToDecimal(int numerator, int denominator) {
+    public static String fractionToDecimal(int numerator, int denominator) {
+        if(numerator == 0)
+            return "0";
         StringBuilder result = new StringBuilder();
-        Set<Integer> set = new TreeSet();
-        result.append(numerator / denominator).append('.');
-        int remainder = numerator % denominator;
+        Set<Long> set = new TreeSet();
+        long nmrt = numerator, dnmnt = denominator;
+        if((nmrt ^ dnmnt) < 0)
+            result.append('-');
+        if(nmrt < 0)
+            nmrt *= -1;
+        if(dnmnt < 0)
+            dnmnt *= -1;
+        result.append(nmrt / dnmnt).append('.');
+        long remainder = nmrt % dnmnt;
         while(remainder != 0){
             if(set.contains(remainder)){
-
+                set.clear();
+                StringBuilder cycDecimal = new StringBuilder();
+                while(remainder != 0){
+                    if(set.contains(remainder)){
+                        int pos = result.indexOf(cycDecimal.toString());
+                        result.insert(pos, '(');
+                        result.insert(pos + cycDecimal.length() + 1, ')');
+                        return result.substring(0, pos + cycDecimal.length() + 2);
+                    }
+                    set.add(remainder);
+                    remainder *= 10;
+                    cycDecimal.append(remainder / dnmnt);
+                    remainder = remainder % dnmnt;
+                }
             }
-
+            set.add(remainder);
+            remainder *= 10;
+            result.append(remainder / dnmnt);
+            remainder = remainder % dnmnt;
         }
+        if(result.charAt(result.length() - 1) == '.')
+            result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(fractionToDecimal(1, 90));
     }
 
 }
